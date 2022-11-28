0eb49b1559fb868f8362f63f05ed0e1541d3d6ad
Leon Dobrzinsky <ldobrzinsky@paloaltonetworks.com>
2022-11-28 16:03:38 +0200

    mem_leak

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -3,13 +3,14 @@ package com.dockerforjavadevelopers.hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.dockerforjavadevelopers.hello.MemoryLeakNoOOMDemo;
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
+    public String index() throws Exception {
+        MemoryLeakNoOOMDemo leaker = new MemoryLeakNoOOMDemo();
+        leaker.start();
         return "Hello World\n";
     }
-    
 }
